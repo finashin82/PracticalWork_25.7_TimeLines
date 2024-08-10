9e0e0a83e1df9eb7d3dffe3d17897fcf8e82f265
finashin82 <finashin82@mail.ru>
2024-08-10 13:23:39 +0900

    Практика готова
    Практика готова

---
+++ PracticalWork_25.7_TimeLine/Assets/Scripts/FlashFireTourch.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class FlashFireTourch : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> tourch = new List<GameObject>();
+
+    [SerializeField] private float timerBetweenFlashes;
+
+    [SerializeField] private AudioSource flashSource;
+
+    private float timerFlash;
+
+    private int lengthArray, i=0;
+
+    private bool isPermissionFlash;
+
+    private void Awake()
+    {
+        lengthArray = tourch.Count;
+
+        timerFlash = timerBetweenFlashes;
+    }
+
+    private void Update()
+    {
+        if (isPermissionFlash)
+        {
+            timerFlash -= Time.deltaTime;
+
+            if (timerFlash < 0)
+            {
+                FlashFireOn();
+
+                flashSource.Play();
+
+                timerFlash = timerBetweenFlashes;
+            }
+        }
+
+        if (i == lengthArray)
+        {
+            isPermissionFlash = false;
+        }
+    }
+
+    public void PermissionFlash()
+    {
+        isPermissionFlash = true;
+    }
+
+    private void FlashFireOn()
+    {
+        if (i < lengthArray)
+        {
+            tourch[i].SetActive(true);
+            i++;
+        }        
+    }   
+}
